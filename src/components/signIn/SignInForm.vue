8a9c32a7e2796bfff93b0f8ebc19ad40a67621d5
Efraim Munthe <efrayanglain@gmail.com>
2023-09-11 10:13:20 +0700

    switch forms

--- src/components/signIn/SignInForm.vue
+++ src/components/signIn/SignInForm.vue
@@ -1,5 +1,61 @@
-<script setup></script>
+<script setup>
+import { computed, ref } from "vue"
+
+const props = defineProps({
+  isLoggingIn: Boolean,
+})
+
+const isSignUp = computed(() => !props.isLoggingIn)
+
+const email = ref("")
+const password = ref("")
+</script>
 
 <template>
-  <div class="signin"></div>
+  <div class="signin">
+    <div class="form-container log-in" v-show="isLoggingIn">
+      <form @submit.prevent="handleLogIn">
+        <label for="login-email">Email</label>
+        <input required type="email" id="login-email" placeholder="Email" v-model="email" />
+
+        <label for="login-password">Password</label>
+        <input
+          required
+          type="password"
+          id="login-password"
+          placeholder="Your password here"
+          v-model="password"
+        />
+        <button type="submit" class="btn submit-button">Log in</button>
+      </form>
+    </div>
+
+    <div class="form-container sign-up" v-show="isSignUp">
+      <form @submit.prevent="handleSignUp">
+        <label for="signup-email">Email</label>
+        <input required type="email" id="signup-email" placeholder="Email" v-model="email" />
+        <label for="signup-password">Password</label>
+        <input
+          required
+          type="password"
+          id="signup-password"
+          placeholder="Your password here"
+          v-model="password"
+        />
+
+        <button type="submit" class="btn submit-button">Sign up</button>
+      </form>
+    </div>
+  </div>
 </template>
+
+<style>
+.signin-form {
+  flex-basis: 50ch;
+}
+
+.submit-button {
+  width: 100%;
+  margin-block: 1rem;
+}
+</style>
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,15 +1,26 @@
+<script setup>
+import { ref } from "vue"
+import SignInForm from "../components/signIn/SignInForm.vue"
+
+const isLoggingIn = ref(false)
+
+function handleSwitchForm() {
+  isLoggingIn.value = !isLoggingIn.value
+}
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>
+      <span v-if="isLoggingIn">Login</span>
+      <span v-else>Sign up</span>
+    </h1>
+
+    <SignInForm :isLoggingIn="isLoggingIn" />
+
+    <button @click="handleSwitchForm" class="btn submit-button">
+      <span v-if="isLoggingIn">Belum punya akun? Sign up</span>
+      <span v-else>Have an account already? Log in</span>
+    </button>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
