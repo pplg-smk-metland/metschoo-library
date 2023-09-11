cc6611c6fb25853b72892dff33e54176865ddc34
Efraim Munthe <efrayanglain@gmail.com>
2023-09-11 09:43:14 +0700

    formatting

---
+++ src/components/BookItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  buku: Object,
+})
+</script>
+
+<template>
+  <li class="book">
+    <h2>{{ buku.judul }}</h2>
+    <p>{{ buku.no_isbn }}</p>
+  </li>
+</template>
+
+<style>
+.book {
+  outline: 2px solid #444;
+  padding: 2rem;
+}
+
+.book + .book {
+  margin-block-start: 1rem;
+}
+</style>
