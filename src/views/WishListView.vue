83c0af62793a9a21f72601fa7fb81bd905b3b501
Efraim Munthe <efrayanglain@gmail.com>
2023-09-14 11:22:45 +0700

    login only view

--- src/views/WishListView.vue
+++ src/views/WishListView.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { useAuthStore } from "../stores/auth.js"
+
+const authStore = useAuthStore()
+</script>
+
 <template>
-  <main class="wishlist"></main>
+  <main class="wishlist">
+    <h1>Wishlist</h1>
+    <p v-if="authStore.session">
+      Kamu sudah login tapi kami belom selesai buat aplikasinya sabar yah
+    </p>
+    <p v-else>Kamu harus login dlu</p>
+  </main>
 </template>
