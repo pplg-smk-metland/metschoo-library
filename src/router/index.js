1c9098c80f4b659bb5d50f83b7d31de31cf27aac
Efraim Munthe <efrayanglain@gmail.com>
2023-09-19 11:30:42 +0700

    add custom link class

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/pustaka",
+    name: "pustaka",
+    component: () => import("../views/PustakaView.vue"),
+  },
+  {
+    path: "/wishlist",
+    name: "wishlist",
+    component: () => import("../views/WishListView.vue"),
+  },
+  {
+    path: "/sign-in",
+    name: "sign-in",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/SignInView.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: () => import("../views/ProfileView.vue"),
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/pustaka",
-      name: "pustaka",
-      component: () => import("../views/PustakaView.vue"),
-    },
-    {
-      path: "/wishlist",
-      name: "wishlist",
-      component: () => import("../views/WishListView.vue"),
-    },
-    {
-      path: "/sign-in",
-      name: "sign-in",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/SignInView.vue"),
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      component: () => import("../views/ProfileView.vue"),
-    },
-  ],
+  routes,
+  linkActiveClass: "active",
 })
 
 export default router
