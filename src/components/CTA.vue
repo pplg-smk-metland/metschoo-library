fc27b36e936c67db9de75eb7e7db44cffa29f254
Efraim Munthe <efrayanglain@gmail.com>
2023-09-19 11:28:50 +0700

    add new CTA component

---
+++ src/components/CTA.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  isButton: Boolean,
+})
+</script>
+
+<template>
+  <button v-if="isButton" class="cta">
+    <slot />
+  </button>
+
+  <a v-else href="#">
+    <slot />
+  </a>
+</template>
+
+<style scoped>
+.cta {
+  outline: 2px solid var(--primary);
+  padding: 1rem 2rem;
+  margin-block: 1rem;
+}
+</style>
