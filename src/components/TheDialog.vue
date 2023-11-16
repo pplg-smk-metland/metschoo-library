a96841a2d5ea09d137d04cb94b27456b9871a262
Efraim Munthe <efrayanglain@gmail.com>
2023-11-16 07:56:25 +0700

    polish dialog component
    polish dialog component
    
    cleanup console.log

--- src/components/TheDialog.vue
+++ src/components/TheDialog.vue
@@ -1,9 +1,49 @@
-<script setup></script>
+<script setup>
+import { watch, ref } from "vue"
+
+const props = defineProps({
+  isOpen: Boolean,
+})
+
+const emit = defineEmits(["dialogClose"])
+
+const myDialog = ref(null)
+
+function openDialog() {
+  myDialog.value.showModal()
+}
+
+function closeDialog() {
+  myDialog.value.close()
+  emit("dialogClose")
+}
+
+watch(
+  () => props.isOpen,
+  (newValue, oldValue) => {
+    // newValue is passed when dialog opens, aka props.isOpen changes to true
+    if (newValue) openDialog()
+  }
+)
+</script>
 
 <template>
-  <dialog>
+  <dialog ref="myDialog" class="dialog" @close="closeDialog">
     <form method="dialog">
-      <p>ini adalah sebuah dialog</p>
+      <button @click="closeModal">close dialog</button>
+      <slot>
+        <p>ini adalah sebuah dialog</p>
+      </slot>
     </form>
   </dialog>
 </template>
+
+<style>
+.dialog {
+  margin: auto;
+}
+
+.dialog::backdrop {
+  backdrop-filter: blur(2rem);
+}
+</style>
