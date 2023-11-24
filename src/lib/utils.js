b4dbb3988fdaa3618edc672b37c93ed40c8d9cf1
Efraim Munthe <efrayanglain@gmail.com>
2023-11-24 12:24:02 +0700

    add placeholder image when not available

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,6 @@
 import { supabase } from "../supabase"
+import { useAuthStore } from "../stores/auth"
+
 export async function ambilGambarBukuDariISBN(isbn) {
   const cdnURL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/Buku`
 
@@ -8,7 +10,8 @@ export async function ambilGambarBukuDariISBN(isbn) {
       .list(`${isbn}/`, { limit: 1, offset: 0, search: isbn })
     if (error) throw error
 
-    return `${cdnURL}/${isbn}/${data[0]?.name}`
+    if (data.length) return `${cdnURL}/${isbn}/${data[0]?.name}`
+    else return "../../assets/Image_not_available.png"
   } catch (err) {
     console.error(err.message)
   }
