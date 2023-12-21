0a7b0c9300682c54d80f88419b2ba7ba35bff5c1
Efraim Munthe <efrayanglain@gmail.com>
2023-12-21 18:29:31 +0700

    get rid of duplicate footer

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,10 +1,8 @@
 <script setup>
 import TheNavbar from "@/components/TheNavbar.vue"
-import TheFooter from "@/components/TheFooter.vue"
 </script>
 
 <template>
   <TheNavbar />
   <RouterView />
-  <TheFooter />
 </template>
