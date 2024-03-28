324975fb933bb56ddf4ac36a9dd14fac15af0a85
Efraim Munthe <efrayanglain@gmail.com>
2024-03-28 15:36:48 +0700

    add category

--- src/views/admin/AdminHalamanBuku.vue
+++ src/views/admin/AdminHalamanBuku.vue
@@ -8,7 +8,18 @@ import CTA from "@/components/CTA.vue"
 const isLoading = ref(false)
 
 const buku = ref({})
+const availableCategories = ref([])
 const router = useRoute()
+
+async function getCategories() {
+  try {
+    const { data, error } = await supabase.from("kategori_buku").select("*")
+    if (error) throw error
+    return data
+  } catch (err) {
+    console.trace(err.message)
+  }
+}
 
 async function ambilBuku() {
   try {
@@ -29,9 +40,9 @@ async function ambilBuku() {
 
 onMounted(async () => {
   buku.value = await ambilBuku()
+  availableCategories.value = await getCategories()
 })
 
-//TODO: add kategori
 async function editBook() {
   try {
     const { error } = await supabase
@@ -45,6 +56,7 @@ async function editBook() {
         alamat_terbit: buku.value.alamat_terbit,
         asal: buku.value.asal,
         jumlah_exspl: buku.value.jumlah_exspl,
+        kategori_id: buku.value.kategori_id,
       })
       .eq("no_isbn", router.params.isbn)
     if (error) throw error
@@ -68,11 +80,12 @@ function toggleFormVisibility() {
   <LoadingSpinner v-if="isLoading" />
 
   <article class="buku" v-else v-show="!formIsVisible">
-    <h1>Data {{ buku.judul }}</h1>
+    <h1>Data {{ buku.judul }} - {{ buku.jumlah_exspl }}</h1>
     <p>{{ buku.penulis }}</p>
+    <p>{{ buku.asal }}</p>
     <p>{{ buku.penerbit }}</p>
     <p>{{ buku.tahun_terbit }} - {{ buku.alamat_terbit }}</p>
-    <!-- <p>{{ buku.kategori_buku.kategori }}</p> -->
+    <p>{{ buku.kategori_buku?.kategori }}</p>
   </article>
 
   <article v-show="formIsVisible">
@@ -81,20 +94,21 @@ function toggleFormVisibility() {
 
     <form class="buku-edit" @submit.prevent="editBook">
       <label for="buku-judul">Judul</label>
-      <input type="text" name="buku-judul" id="buku-judul" v-model="buku.judul" />
+      <input type="text" name="buku-judul" id="buku-judul" required v-model="buku.judul" />
       <label for="buku-asal">Asal</label>
-      <input type="text" name="buku-asal" id="buku-asal" v-model="buku.asal" />
+      <input type="text" name="buku-asal" id="buku-asal" required v-model="buku.asal" />
+      <label for="buku-penulis">ISBN</label>
+      <input type="text" name="buku-isbn" id="buku-isbn" required v-model="buku.no_isbn" />
       <label for="buku-penulis">Penulis</label>
-      <input type="text" name="buku-isbn" id="buku-isbn" v-model="buku.no_isbn" />
-      <label for="buku-penulis">Penulis</label>
-      <input type="text" name="buku-penulis" id="buku-penulis" v-model="buku.penulis" />
+      <input type="text" name="buku-penulis" id="buku-penulis" required v-model="buku.penulis" />
       <label for="buku-penerbit">Penerbit</label>
-      <input type="text" name="buku-penerbit" id="buku-penerbit" v-model="buku.penerbit" />
+      <input type="text" name="buku-penerbit" id="buku-penerbit" required v-model="buku.penerbit" />
       <label for="buku-tahun-terbit">Tahun terbit</label>
       <input
         type="text"
         name="buku-tahun-terbit"
         id="buku-tahun-terbit"
+        required
         v-model="buku.tahun_terbit"
       />
       <label for="buku-alamat-terbit">Alamat terbit</label>
@@ -102,6 +116,7 @@ function toggleFormVisibility() {
         type="text"
         name="buku-alamat-terbit"
         id="buku-alamat-terbit"
+        required
         v-model="buku.alamat_terbit"
       />
       <label for="buku-jumlah">Jumlah</label>
@@ -109,17 +124,20 @@ function toggleFormVisibility() {
         type="number"
         name="buku-jumlah"
         id="buku-jumlah"
+        min="0"
         max="10000"
+        required
         v-model="buku.jumlah_exspl"
       />
-      <!-- <label for="buku-kategori">Kategori</label> -->
-      <!-- <input -->
-      <!--   type="text" -->
-      <!--   name="buku-kategori" -->
-      <!--   id="buku-kategori" -->
-      <!--   v-model="buku.kategori_buku.kategori" -->
-      <!-- /> -->
-      <CTA>Save</CTA>
+      <label for="buku-kategori">Kategori</label>
+      <select name="buku-kategori" id="buku-kategori" v-model="buku.kategori_id" required>
+        <option value="" disabled>Please select one</option>
+        <option v-for="category in availableCategories" :key="category.id" :value="category.id">
+          {{ category.id }} - {{ category.kategori }}
+        </option>
+      </select>
+
+      <CTA>Save changes</CTA>
     </form>
   </article>
 
