2e4e5cdc261933166b4755281e7ad6ad9cb5fe1b
Efraim Munthe <efrayanglain@gmail.com>
2024-03-29 12:35:28 +0700

    add search to admin book page

--- src/views/admin/AdminDataBuku.vue
+++ src/views/admin/AdminDataBuku.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue"
 import { supabase } from "@/lib/supabase"
+import CTA from "@/components/CTA.vue"
 
 const daftarBuku = ref([])
 
@@ -13,16 +14,66 @@ async function ambilBuku() {
   return data
 }
 
+const availableCategories = ref([])
+const selectedCategory = ref(null)
+
+async function getCategories() {
+  try {
+    const { data, error } = await supabase.from("kategori_buku").select("*")
+    if (error) throw error
+    return data
+  } catch (err) {
+    console.trace(err.message)
+  }
+}
+
+async function searchBooks() {
+  try {
+    const { data, error } = await supabase
+      .from("buku")
+      .select(`no_isbn, judul, penulis, kategori_buku(kategori)`)
+      .textSearch("judul", searchTerm.value, { type: "websearch" })
+      .eq("kategori_id", selectedCategory.value)
+      .limit(30)
+    if (error) throw error
+    daftarBuku.value = data
+  } catch (err) {
+    console.trace(err.message)
+  }
+}
+
 onMounted(async () => {
   daftarBuku.value = await ambilBuku()
+  availableCategories.value = await getCategories()
 })
+
+const searchTerm = ref("")
 </script>
 
 <template>
   <h1>Data buku</h1>
 
+  <form @submit.prevent="searchBooks">
+    <input
+      type="text"
+      name="search-term"
+      id="search-term"
+      v-model="searchTerm"
+      placeholder="search for book"
+      required
+    />
+    <select name="search-category" id="search-category" v-model="selectedCategory" required>
+      <option value="" disabled>Select one</option>
+      <option v-for="category in availableCategories" :key="category.id" :value="category.id">
+        {{ category.id }} - {{ category.kategori }}
+      </option>
+    </select>
+    <CTA>Search</CTA>
+  </form>
+
   <ul>
-    <li v-for="buku in daftarBuku" :key="buku.no_isbn">
+    <li v-if="!daftarBuku.length">No book found</li>
+    <li v-for="buku in daftarBuku" :key="buku.no_isbn" v-else>
       <routerLink :to="`/admin/buku/${buku.no_isbn}`">
         <p>{{ buku.judul }}</p>
         <p>{{ buku.penulis }}</p>
