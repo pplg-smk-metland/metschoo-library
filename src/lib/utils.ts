36261c974a24fe772aee928216b601190f41226f
Efraim Munthe <efrayanglain@gmail.com>
2024-04-16 15:48:24 +0700

    update error handling when getting book image

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,17 +5,15 @@ import type { PostgrestError } from "@supabase/supabase-js"
 export async function ambilGambarBukuDariISBN(isbn: string) {
   const cdnURL = `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/Buku`
 
-  try {
-    const { data, error } = await supabase.storage
-      .from("Buku")
-      .list(`${isbn}/`, { limit: 1, offset: 0, search: isbn })
-    if (error) throw error
-
-    if (data.length) return `${cdnURL}/${isbn}/${data[0]?.name}`
-    else return "../../assets/Image_not_available.png"
-  } catch (err) {
-    console.error((err as PostgrestError).message)
+  const { data, error } = await supabase.storage
+    .from("Buku")
+    .list(`${isbn}/`, { limit: 1, offset: 0, search: isbn })
+  if (!data?.length || error) {
+    if (error) console.error(error.message)
+    return "../../assets/Image_not_available.png"
   }
+
+  return `${cdnURL}/${isbn}/${data[0]?.name}`
 }
 
 export async function pinjamBukuDariISBN(isbn: string) {
