0609cf9987dcea32b6a9e8a8a5f859edf71cd23c
Efraim Munthe <efrayanglain@gmail.com>
2024-04-19 12:22:18 +0700

    search

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { onMounted, provide, ref, type InjectionKey, type Ref } from "vue"
+import { onMounted, provide, ref } from "vue"
 import { useAuthStore } from "@/stores/auth"
 import { searchTermKey } from "@/stores/search"
 import { RouterView } from "vue-router"
 import TheFooter from "@/components/TheFooter.vue"
 
-const searchTerm = ref("")
+provide(searchTermKey, ref(""))
 
 provide(searchTermKey, searchTerm)
 
