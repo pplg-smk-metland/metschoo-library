70cceaac6f5cb62795b72bebc93d02beb6bf1da9
Efraim Munthe <efrayanglain@gmail.com>
2024-05-10 12:43:34 +0700

    fix navigation error

--- src/views/profile/ProfileEdit.vue
+++ src/views/profile/ProfileEdit.vue
@@ -10,7 +10,14 @@ import CTA from "@/components/CTA.vue"
 import type { Pengguna } from "@/types"
 
 const { dialog } = useDialog()
-const dataPengguna = ref<Pengguna | null>()
+
+const dataPengguna = ref<Pengguna | null>(null)
+const authStore = useAuthStore()
+
+onMounted(async () => {
+  const data = await authStore.getProfile()
+  dataPengguna.value = data
+})
 
 async function updateUserInfo() {
   try {
@@ -20,13 +27,6 @@ async function updateUserInfo() {
     console.error((err as PostgrestError).message)
   }
 }
-
-const authStore = useAuthStore()
-
-onMounted(async () => {
-  const data = await authStore.getProfile()
-  if (data) dataPengguna.value = data
-})
 </script>
 
 <template>
@@ -36,7 +36,7 @@ onMounted(async () => {
       <routerLink :to="{ name: 'profile' }">Kembali</routerLink>
     </section>
 
-    <section class="profile">
+    <section class="profile" v-if="dataPengguna">
       <figure class="profile__picture-wrapper">
         <img class="profile__picture" src="@/assets/profilepicture.svg" alt="Foto kamu disini" />
         <CTA>Edit foto profil</CTA>
@@ -44,22 +44,27 @@ onMounted(async () => {
 
       <form class="profile__details" @submit.prevent="updateUserInfo">
         <label for="name">Nama</label>
-        <input type="text" placeholder="Masukan Nama" v-model="dataPengguna?.nama" />
+        <input type="text" placeholder="Masukan Nama" v-model="dataPengguna.nama" />
 
         <label for="kelas">Kelas</label>
-        <select name="kelas" id="kelas" v-model="dataPengguna?.kelas">
+        <select name="kelas" id="kelas" v-model="dataPengguna.kelas">
           <option value="X">X</option>
           <option value="XI">XI</option>
           <option value="XII">XII</option>
         </select>
 
         <label for="jurusan">Jurusan</label>
-        <input type="text" placeholder="Masukkan Jurusan" v-model="dataPengguna?.jurusan" />
+        <input type="text" placeholder="Masukkan Jurusan" v-model="dataPengguna.jurusan" />
 
         <div class="button-container">
           <CTA :fill="true">Edit profil</CTA>
         </div>
       </form>
+    </section>
+
+    <section v-else>
+      <h1>Pengguna tidak ditemukan!</h1>
+      <p>silahkan coba lagi.</p>
     </section>
 
     <TheDialog :is-open="dialog.isOpen" @dialog-close="dialog.close()">
