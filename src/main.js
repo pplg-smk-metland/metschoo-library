e9bf0fb0362ed528fdfcbde52962f3c4c3b0aa5f
Efraim Munthe <efrayanglain@gmail.com>
2024-05-17 19:02:21 +0700

    add primevue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./assets/main.css"
 
 import { createApp } from "vue"
 import { createPinia } from "pinia"
+import PrimeVue from "primevue/config"
 
 import App from "./App.vue"
 import router from "./router"
@@ -10,5 +11,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  unstyled: true,
+})
 
 app.mount("#app")
