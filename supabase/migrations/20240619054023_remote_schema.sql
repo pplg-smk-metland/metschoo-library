3bb39a0c630dda50b62ade38b056b8f43d81bdca
Efraim Munthe <efrayanglain@gmail.com>
2024-06-19 14:30:45 +0700

    fix migration file to include missing functions

--- supabase/migrations/20240619054023_remote_schema.sql
+++ supabase/migrations/20240619054023_remote_schema.sql
@@ -60,6 +60,11 @@ begin
 end;
 $$;
 
+drop trigger if exists on_auth_user_created on auth.users;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
 ALTER FUNCTION "public"."handle_update_user"() OWNER TO "postgres";
 
 SET default_tablespace = '';
