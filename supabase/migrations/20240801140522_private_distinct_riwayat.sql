5f584cac99e5178c7887c6645f839a27a667ba58
Efraim Munthe <efrayanglain@gmail.com>
2024-08-06 11:36:57 +0700

    Patch (#15)
    * add class header
    
    * patch unique history db migration

--- supabase/migrations/20240801140522_private_distinct_riwayat.sql
+++ supabase/migrations/20240801140522_private_distinct_riwayat.sql
@@ -1,7 +1,11 @@
+drop view if exists public.distinct_riwayat;
+
+-- new
+drop view public.distinct_riwayat;
 create or replace view public.distinct_riwayat with ("security_invoker" = 'on') as
   select distinct "peminjaman".state_id,
-    public."buku".* as buku
+    "buku".*::"public"."buku" AS "buku"
   from (public."peminjaman"
-  join public."buku"
-  on ("peminjaman".no_isbn = "buku".no_isbn))
+    join public."buku"
+      on ("peminjaman".no_isbn = "buku".no_isbn))
   where peminjaman.state_id = ANY(ARRAY[5, 6]) and (auth.uid() = peminjaman.user_id);
