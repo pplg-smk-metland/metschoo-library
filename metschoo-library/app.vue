ff253b8d116dfc5bc934aa8742bdc10f9449b130
Efraim Munthe <efrayanglain@gmail.com>
2024-08-20 21:04:12 +0700

    make navbar and default layout

--- metschoo-library/app.vue
+++ metschoo-library/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <NuxtLayout>
     <NuxtPage />
-  </main>
+  </NuxtLayout>
 </template>
--- metschoo-library/pages/index.vue
+++ metschoo-library/pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "default",
+})
+</script>
+
 <template>
   <h1>hai gais</h1>
 </template>
