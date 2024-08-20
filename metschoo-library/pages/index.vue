380a13f6e9c590faae1b763f1741710221bd3385
Efraim Munthe <efrayanglain@gmail.com>
2024-08-20 21:38:36 +0700

    add eslint and prettier

--- metschoo-library/pages/index.vue
+++ metschoo-library/pages/index.vue
@@ -5,5 +5,20 @@ definePageMeta({
 </script>
 
 <template>
-  <h1>hai gais</h1>
+  <TheHeader>
+    <template #header-heading>
+      <!-- Halo, <span v-if="user">{{ user.nama }}.</span> -->
+      <span>Halo kamu!</span>
+    </template>
+  </TheHeader>
+
+  <section class="main-section">
+    <h2>Rekomendasi</h2>
+    <BookList :type-id="1" />
+  </section>
+
+  <section class="main-section">
+    <h2>Koleksi</h2>
+    <BookList :type-id="2" />
+  </section>
 </template>
