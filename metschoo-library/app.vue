cd522a1c96097379d340a846f91c813787938efb
Efraim Munthe <efrayanglain@gmail.com>
2024-08-20 21:06:02 +0700

    add some meta tags

--- metschoo-library/app.vue
+++ metschoo-library/app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Metschoo Library",
+  meta: [
+    {
+      name: "description",
+      content: "Aplikasi Perpustakan Metschoo.",
+    },
+  ],
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
