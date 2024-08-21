03e5b97806782494678a4b74164b241d5ff42809
Efraim Munthe <efraimmunthe101@gmail.com>
2024-08-21 08:52:21 +0700

    set up supabase nuxt integration

--- metschoo-library/nuxt.config.ts
+++ metschoo-library/nuxt.config.ts
@@ -2,8 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@primevue/nuxt-module", "@nuxt/eslint"],
+  modules: ["@pinia/nuxt", "@primevue/nuxt-module", "@nuxt/eslint", "@nuxtjs/supabase"],
   srcDir: ".",
   css: ["~/assets/base.css", "~/assets/main.css"],
+  supabase: {
+    redirect: false,
+    redirectOptions: {
+      login: "/login",
+      callback: "/",
+    },
+  }
 })
 
