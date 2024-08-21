2f9a3cb390037d28dc371b46312804d083a84b54
Efraim Munthe <efraimmunthe101@gmail.com>
2024-08-21 09:43:37 +0700

    added unplugin icons

--- metschoo-library/nuxt.config.ts
+++ metschoo-library/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@primevue/nuxt-module", "@nuxt/eslint", "@nuxtjs/supabase"],
+  modules: ["@pinia/nuxt", "@primevue/nuxt-module", "@nuxt/eslint", "@nuxtjs/supabase", "unplugin-icons/nuxt"],
   srcDir: ".",
   css: ["~/assets/base.css", "~/assets/main.css"],
   supabase: {
