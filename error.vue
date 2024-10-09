02cb5d06bc93873168827c2f9426ddad9f8a2fc1
Efraim <efrayanglain@gmail.com>
2024-10-09 14:05:00 +0700

    feat: add a new page for handling errors

---
+++ error.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app"
+
+const themeStore = useThemeStore()
+
+useHead({
+  title: "404 Gak Ketemu",
+  htmlAttrs: {
+    class: computed(() => themeStore.theme),
+  },
+  bodyAttrs: {
+    class: "bg-surface-100/80 dark:bg-surface-900 dark:text-gray-300",
+  },
+})
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+
+const handleError = () => clearError({ redirect: "/" })
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <section class="flex flex-col items-center">
+      <template v-if="error?.statusCode === 404">
+        <h1>Halamannya gak ketemu!</h1>
+        <p>Maaf ya. Balik lagi aja ke beranda gih</p>
+      </template>
+
+      <template v-else>
+        <h1>Ada yang salah!</h1>
+        <p>Ga tau deh kenapa. Laporkan ke admin atau kembali ke beranda</p>
+      </template>
+
+      <CTA label="kembali ke beranda" @click="handleError" />
+    </section>
+  </NuxtLayout>
+</template>
