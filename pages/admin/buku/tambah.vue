86e51e24d46686ddd9ccf92f05ad80cbf185ca22
Efraim Munthe <efrayanglain@gmail.com>
2025-02-01 04:50:07 +0000

    feat: validate form in buku/tambah

--- pages/admin/buku/tambah.vue
+++ pages/admin/buku/tambah.vue
@@ -2,13 +2,13 @@
 import { useBuku, useDialog } from "@/composables"
 import Select from "primevue/select"
 import { StorageError } from "@supabase/storage-js"
-import type { Buku } from "@/types"
 import type { PostgrestError } from "@supabase/supabase-js"
 import IconArrowLeft from "~icons/mdi/arrow-left"
 import type { Database } from "~/types/database.types.ts"
 import { InputText, InputNumber, FileUpload } from "primevue"
 import { zodResolver } from "@primevue/forms/resolvers/zod"
 import { z } from "zod"
+import type { FormSubmitEvent } from "@primevue/forms"
 
 useHead({
   title: "Tambah Buku",
@@ -19,7 +19,7 @@ definePageMeta({
 })
 
 const supabase = useSupabaseClient<Database>()
-const { buku } = useBuku()
+const { buku: formData } = useBuku()
 
 const isLoading = ref(false)
 const { dialog } = useDialog()
@@ -28,38 +28,58 @@ const { dialog: errDialog } = useDialog()
 const { newImage, previewURL, previewImage } = usePreviewImage()
 
 async function uploadBookImage(isbn: string, file: File) {
-  if (!buku.value) return console.trace("buku gak ada????")
+  if (!formData.value) return console.trace("buku gak ada????")
 
-  buku.value.image = `public/${isbn}`
-  const { error } = await supabase.storage.from("Buku").upload(buku.value.image, file, {
+  formData.value.image = `public/${isbn}`
+  const { error } = await supabase.storage.from("Buku").upload(formData.value.image, file, {
     upsert: true,
   })
   return error
 }
 
 const schema = z.object({
-  judul: z.string(),
-  no_isbn: z.string(),
-  kategori_id: z.number(),
-  asal: z.string(),
+  judul: z.string().nonempty("judul tidak boleh kosong."),
+  no_isbn: z.string().nonempty("isbn tidak boleh kosong."),
+  image: z.string().nullable(),
+  kategori_id: z.number({ message: "buku harus memiliki kategori." }),
+  asal: z.string().default("-"),
   jumlah_exspl: z
-    .number()
+    .number({ message: "jumlah harus berupa angka." })
     .min(0, "jumlah harus 0 atau lebih.")
     .max(10000, "banyak amat sih ga mungkin lah bukunya segitu."),
-  penerbit: z.string(),
-  alamat_terbit: z.string(),
-  tahun_terbit: z.string().regex(/\d{0,4}/, { message: "tahun terbit harus memiliki 4 digit." }),
-  penulis: z.string(),
+  penerbit: z.string().nonempty("penerbit tidak boleh kosong."),
+  alamat_terbit: z.string().nonempty("alamat penerbit tidak boleh kosong."),
+  tahun_terbit: z
+    .string()
+    .regex(/^\d{0,4}$/, { message: "tahun terbit harus berupa angka 0 sampai 4 digit." }),
+  penulis: z.string().nonempty("penulis tidak boleh kosong"),
 })
 
 const resolver = zodResolver(schema)
 
-async function addNewBook(buku: Buku) {
+async function addNewBook({ valid, values }: FormSubmitEvent) {
+  if (!valid) return
+
   isLoading.value = true
+
+  const { data: buku, success, error } = schema.safeParse(values)
+  if (!buku || !success) {
+    isLoading.value = false
+
+    console.log(buku, success, error)
+
+    return toast.add({
+      severity: "error",
+      summary: "Gagal menambahkan buku",
+      detail: "gagal menambahkan buku. Silahkan refresh atau coba lagi dalam beberapa saat.",
+      life: 10000,
+    })
+  }
 
   const { no_isbn } = buku
 
   try {
+    // upload image if admin puts an image
     if (newImage.value) {
       const uploadError = await uploadBookImage(no_isbn, newImage.value)
       if (uploadError) throw uploadError
@@ -135,11 +155,7 @@ const router = useRouter()
     </CTA>
   </PageHeader>
 
-  <section v-if="!buku">
-    <p>Ada kesalahan saat mengambil data buku. Silahkan coba lagi</p>
-  </section>
-
-  <section v-else class="flex flex-col justify-center lg:flex-row gap-4 lg:gap-6 main-section">
+  <section class="flex flex-col justify-center lg:flex-row gap-4 lg:gap-6 main-section">
     <figure
       class="mx-auto lg:m-0 max-w-60 aspect-[2/3] align-self-start border-2 border-gray-300 dark:border-gray-700 rounded-lg overflow-hidden flex justify-center items-center bg-gray-100 dark:bg-gray-800"
     >
@@ -149,7 +165,6 @@ const router = useRouter()
         width="150"
         height="800"
         class="size-full object-cover aspect-auto"
-        :alt="`gambar buku ${buku?.judul}`"
       />
       <p v-else class="text-center text-gray-600 dark:text-gray-400">
         Gambar buku akan muncul di sini.
@@ -158,20 +173,20 @@ const router = useRouter()
 
     <Form
       v-slot="$form"
-      :resolver="resolver"
+      :resolver
       :validate-on-blur="true"
       :validate-on-submit="true"
       :validate-on-value-update="false"
       class="grid grid-cols-1 sm:grid-cols-2 gap-4"
-      @submit="addNewBook(buku)"
+      @submit="addNewBook"
     >
       <label for="buku-gambar" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Gambar buku</span>
         <FileUpload
           id="buku-gambar"
           ref="buku-gambar"
+          name="image"
           mode="basic"
-          name="buku-gambar"
           accept="image/*"
           class="rounded-md w-full border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
           custom-upload
@@ -191,6 +206,10 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.judul?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.judul?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-isbn" class="flex flex-col">
@@ -203,12 +222,16 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.no_isbn?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.no_isbn?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-kategori" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Kategori</span>
         <Select
-          v-model="buku.kategori_id"
+          name="kategori_id"
           placeholder="Pilih kategori"
           :options="availableCategories ?? []"
           checkmark
@@ -217,6 +240,10 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.kategori_id?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.kategori_id?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-penulis" class="flex flex-col">
@@ -229,6 +256,9 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+        <Message v-if="$form.penulis?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.penulis?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-penerbit" class="flex flex-col">
@@ -241,6 +271,9 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+        <Message v-if="$form.penerbit?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.penerbit?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-tahun-terbit" class="flex flex-col">
@@ -253,6 +286,10 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.tahun_terbit?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.tahun_terbit?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-alamat-terbit" class="flex flex-col">
@@ -265,6 +302,10 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.alamat_terbit?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.alamat_terbit?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-asal" class="flex flex-col">
@@ -277,6 +318,10 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.asal?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.asal?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-jumlah" class="flex flex-col">
@@ -288,6 +333,10 @@ const router = useRouter()
           fluid
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.jumlah_exspl?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.jumlah_exspl?.error.message }}
+        </Message>
       </label>
 
       <CTA
@@ -303,13 +352,12 @@ const router = useRouter()
         <p>{{ errDialog.message }}</p>
       </TheDialog>
 
-      <TheDialog
-        :is-open="dialog.isOpen"
-        @dialog-close="router.push(`/admin/buku/${buku.no_isbn}`)"
-      >
+      <TheDialog :is-open="dialog.isOpen" @dialog-close="router.push(`/admin/buku/`)">
         <h2>Sukses!</h2>
         <p>{{ dialog.message }}</p>
       </TheDialog>
     </Form>
   </section>
+
+  <Toast />
 </template>
