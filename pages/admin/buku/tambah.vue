a779bf8c216d9bb6c789b26cf994d6a885168c0c
Efraim Munthe <efrayanglain@gmail.com>
2025-02-02 15:23:25 +0700

    feat/form-validation (#97)
    * feat: validate profil edit form
    
    * feat: admin tambah buku wip
    
    * feat: validate form in buku/tambah
    
    * export to lib for reuse
    
    * fix: image upload not working
    
    * feat: add validation on buku edit form
    
    * refactor: extract upload image into separate file
    
    * ui: make style for simple message component

--- pages/admin/buku/tambah.vue
+++ pages/admin/buku/tambah.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { useBuku, useDialog } from "@/composables"
+import { useDialog } from "@/composables"
 import Select from "primevue/select"
 import { StorageError } from "@supabase/storage-js"
-import type { Buku } from "@/types"
 import type { PostgrestError } from "@supabase/supabase-js"
 import IconArrowLeft from "~icons/mdi/arrow-left"
-import type { Database } from "~/types/database.types.ts"
 import { InputText, InputNumber, FileUpload } from "primevue"
+import type { FormSubmitEvent } from "@primevue/forms"
+import { schema, resolver, uploadBookImage } from "~/lib/buku"
 
 useHead({
   title: "Tambah Buku",
@@ -16,37 +16,39 @@ definePageMeta({
   layout: "admin",
 })
 
-const supabase = useSupabaseClient<Database>()
-const { buku } = useBuku()
-
 const isLoading = ref(false)
 const { dialog } = useDialog()
 const { dialog: errDialog } = useDialog()
 
 const { newImage, previewURL, previewImage } = usePreviewImage()
 
-async function uploadBookImage(isbn: string, file: File) {
-  if (!buku.value) return console.trace("buku gak ada????")
+async function addNewBook({ valid, values }: FormSubmitEvent) {
+  if (!valid) return
 
-  buku.value.image = `public/${isbn}`
-  const { error } = await supabase.storage.from("Buku").upload(buku.value.image, file, {
-    upsert: true,
-  })
-  return error
-}
-
-async function addNewBook(buku: Buku) {
   isLoading.value = true
 
-  const { no_isbn } = buku
+  const { data: buku, success, error } = schema.safeParse(values)
+  if (!buku || (buku && !success)) {
+    isLoading.value = false
+
+    console.log(error)
+
+    return toast.add({
+      severity: "error",
+      summary: "Gagal menambahkan buku",
+      detail: "gagal menambahkan buku. Silahkan refresh atau coba lagi dalam beberapa saat.",
+      life: 10000,
+    })
+  }
 
   try {
+    // upload image if admin puts an image
     if (newImage.value) {
-      const uploadError = await uploadBookImage(no_isbn, newImage.value)
+      const uploadError = await uploadBookImage(buku.no_isbn, newImage.value)
       if (uploadError) throw uploadError
     }
 
-    const insertError = await insertBookData(buku)
+    const insertError = await insertBookData({ ...buku, image: `public/${buku.no_isbn}` })
     if (insertError) throw insertError
 
     dialog.value.open("Buku berhasil ditambahkan!")
@@ -116,11 +118,7 @@ const router = useRouter()
     </CTA>
   </PageHeader>
 
-  <section v-if="!buku">
-    <p>Ada kesalahan saat mengambil data buku. Silahkan coba lagi</p>
-  </section>
-
-  <section v-else class="flex flex-col justify-center lg:flex-row gap-4 lg:gap-6 main-section">
+  <section class="flex flex-col justify-center lg:flex-row gap-4 lg:gap-6 main-section">
     <figure
       class="mx-auto lg:m-0 max-w-60 aspect-[2/3] align-self-start border-2 border-gray-300 dark:border-gray-700 rounded-lg overflow-hidden flex justify-center items-center bg-gray-100 dark:bg-gray-800"
     >
@@ -130,21 +128,28 @@ const router = useRouter()
         width="150"
         height="800"
         class="size-full object-cover aspect-auto"
-        :alt="`gambar buku ${buku?.judul}`"
       />
       <p v-else class="text-center text-gray-600 dark:text-gray-400">
         Gambar buku akan muncul di sini.
       </p>
     </figure>
 
-    <form class="grid grid-cols-1 sm:grid-cols-2 gap-4" @submit.prevent="addNewBook(buku)">
+    <Form
+      v-slot="$form"
+      :resolver
+      :validate-on-blur="true"
+      :validate-on-submit="true"
+      :validate-on-value-update="false"
+      class="grid grid-cols-1 sm:grid-cols-2 gap-4"
+      @submit="addNewBook"
+    >
       <label for="buku-gambar" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Gambar buku</span>
         <FileUpload
           id="buku-gambar"
           ref="buku-gambar"
+          name="image"
           mode="basic"
-          name="buku-gambar"
           accept="image/*"
           class="rounded-md w-full border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
           custom-upload
@@ -158,128 +163,143 @@ const router = useRouter()
         <span class="font-semibold text-gray-700 dark:text-gray-300">Judul</span>
         <InputText
           id="buku-judul"
-          v-model="buku.judul"
           type="text"
-          name="buku-judul"
+          name="judul"
           placeholder="judul buku"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.judul?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.judul?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-isbn" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">ISBN</span>
         <InputText
           id="buku-isbn"
-          v-model="buku.no_isbn"
           type="text"
-          name="buku-isbn"
+          name="no_isbn"
           placeholder="ISBN buku"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.no_isbn?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.no_isbn?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-kategori" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Kategori</span>
         <Select
-          v-model="buku.kategori_id"
+          name="kategori_id"
           placeholder="Pilih kategori"
           :options="availableCategories ?? []"
           checkmark
           option-label="kategori"
           option-value="id"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.kategori_id?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.kategori_id?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-penulis" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Penulis</span>
         <InputText
           id="buku-penulis"
-          v-model="buku.penulis"
           type="text"
-          name="buku-penulis"
+          name="penulis"
           placeholder="penulis buku"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+        <Message v-if="$form.penulis?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.penulis?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-penerbit" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Penerbit</span>
         <InputText
           id="buku-penerbit"
-          v-model="buku.penerbit"
           type="text"
-          name="buku-penerbit"
+          name="penerbit"
           placeholder="penerbit"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+        <Message v-if="$form.penerbit?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.penerbit?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-tahun-terbit" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Tahun terbit</span>
         <InputText
           id="buku-tahun-terbit"
-          v-model="buku.tahun_terbit"
           type="text"
-          name="buku-tahun-terbit"
+          name="tahun_terbit"
           placeholder="tahun terbit"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.tahun_terbit?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.tahun_terbit?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-alamat-terbit" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Alamat terbit</span>
         <InputText
           id="buku-alamat-terbit"
-          v-model="buku.alamat_terbit"
           type="text"
-          name="buku-alamat-terbit"
+          name="alamat_terbit"
           placeholder="alamat terbit"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.alamat_terbit?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.alamat_terbit?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-asal" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Asal</span>
         <InputText
           id="buku-asal"
-          v-model="buku.asal"
           type="text"
-          name="buku-asal"
+          name="asal"
           placeholder="asal buku"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.asal?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.asal?.error.message }}
+        </Message>
       </label>
 
       <label for="buku-jumlah" class="flex flex-col">
         <span class="font-semibold text-gray-700 dark:text-gray-300">Jumlah</span>
         <InputNumber
           id="buku-jumlah"
-          v-model="buku.jumlah_exspl"
-          :min="0"
-          :max="10000"
-          name="buku-jumlah"
+          name="jumlah_exspl"
           placeholder="jumlah buku"
           fluid
-          required
           class="rounded-md border-gray-300 dark:border-gray-700 focus:ring-2 focus:ring-green-500 focus:outline-none"
         />
+
+        <Message v-if="$form.jumlah_exspl?.invalid" severity="error" size="small" variant="simple">
+          {{ $form.jumlah_exspl?.error.message }}
+        </Message>
       </label>
 
       <CTA
@@ -295,13 +315,12 @@ const router = useRouter()
         <p>{{ errDialog.message }}</p>
       </TheDialog>
 
-      <TheDialog
-        :is-open="dialog.isOpen"
-        @dialog-close="router.push(`/admin/buku/${buku.no_isbn}`)"
-      >
+      <TheDialog :is-open="dialog.isOpen" @dialog-close="router.push(`/admin/buku/`)">
         <h2>Sukses!</h2>
         <p>{{ dialog.message }}</p>
       </TheDialog>
-    </form>
+    </Form>
   </section>
+
+  <Toast />
 </template>
