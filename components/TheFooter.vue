907f303b78f2196efc6b04bd88743f35bb20e6fa
Efraim Munthe <efrayanglain@gmail.com>
2025-02-03 16:57:08 +0700

    ui: change footer background color

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="dark:bg-surface-700">
+  <footer class="bg-primary-700 text-gray-200/90 dark:bg-surface-700">
     <p>PPLG Angkatan 9, {{ new Date().getFullYear() }}.</p>
     <a class="block w-fit" href="https://github.com/pplg-smkmetland/metschoo-library">
       This website is on Github
